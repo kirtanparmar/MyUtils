03a5b0b4bdbac1a77ad08c4a60d199463a69c185
kirtanparmar <47482389+kirtanparmar@users.noreply.github.com>
2020-08-21 12:01:11 +0530

    Delete OnPermissionReceived.java

--- myUtils/OnPermissionReceived.java
+++
@@ -1,7 +0,0 @@
-package com.iconflux.brokingbulls.myUtils;
-
-import com.iconflux.brokingbulls.models.PropertyProjectPermission;
-
-public interface OnPermissionReceived {
-    void postData(PropertyProjectPermission model);
-}
